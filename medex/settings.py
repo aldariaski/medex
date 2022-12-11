deac5bc5ce01685c9cd9fa485854081574d97f2d
aldariaski <aldrianyf@gmail.com>
2022-12-11 20:25:44 +0700

    Update settings.py

--- medex/settings.py
+++ medex/settings.py
@@ -139,3 +139,5 @@ STATICFILES_DIRS = [
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+CSRF_TRUSTED_ORIGINS = ['*', "django-server-production-a05b.up.railway.app", '.localhost', '127.0.0.1', '[::1]']
